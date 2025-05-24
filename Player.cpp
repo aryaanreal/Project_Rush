83f5ea26059ed8192c92a2549d7a549c0fd999da
jithin <jithintomy2005@gmail.com>
2025-05-24 16:19:30 +0930

    Add initial assets and project structure

---
+++ Player.cpp
@@ -0,0 +1,14 @@
+#include "Player.h"
+
+//constructor sets initial position, speed, and texture
+Player::Player(float startX, float startY, SDL_Tecture* tex)
+    :x(startX), y(startY), speed(5.0f), texture(tex) {}
+// handles players movement (to be done later)
+void Player::move() {
+    //add keyboard logic later
+}
+//draws the player sprite to the screen
+void Player::draw(SDL_Renderer*renderer){
+    SDL_Rect dst = { (int)x, (int)y, 64, 64}; //set position and size
+    SDL_RenderCopy(renderer, texture, nullptr, &dst); //render the texture
+}
