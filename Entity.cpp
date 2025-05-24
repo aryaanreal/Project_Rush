1a56f5f6034548023f3c675a7781313b0b20024e
aryaanreal <aryaantalukder@gmail.com>
2025-05-24 17:19:33 +0930

    Added entity that has the overall position and movement which will be inherited to other classes

---
+++ Entity.cpp
@@ -0,0 +1,6 @@
+#include "Entity.h"
+
+//constructor implementation
+
+Entity::Entity(float x, float y, float speed)
+    : x(x) , y(y), speed(speed){}
---
+++ Entity.h
@@ -0,0 +1,24 @@
+#pragma once      //ensures this file is only included once during compilation
+
+#include <SDL.h>
+
+//abstrach base classes for all game objects (players, enemies, bullets etc)
+
+class Entity {
+  public:
+      float x, y;   //position on screen
+      float speed;  //movement speed
+
+      //constructor that initializes position and speed
+      Entity(float x, float y, float speed);
+
+      //all entities must define how they move
+      virtual void move() = 0;
+
+      //all entities must define how they draw themselves
+      virtual void draw(SDL_Renderer*  renderer) = 0;
+
+      //destructor for cleanup
+      virtual ~Entity()  = default;
+  
+};
