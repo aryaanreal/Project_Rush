1eb9e168425c43ccae87315f817d8201ad66393d
aryaanreal <aryaantalukder@gmail.com>
2025-05-25 15:56:48 +0930

    Fixed a mistake

--- Player.cpp
+++ Player.cpp
@@ -32,6 +32,10 @@ void Player::handleInput(const Uint8* keystate) {
     if (y < 0) y = 0;
     if (y > 500 - 64) y = 500-64;     //the player will be 64 bit in y too
 }
+
+void Player::move() {
+    //will add later
+}
 //draws the player sprite to the screen
 void Player::draw(SDL_Renderer*renderer){
     SDL_Rect dst = { (int)x, (int)y, 64, 64}; //set position and size
