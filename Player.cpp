74d42914a6d1380d61175eee5aa1b533e046aba7
aryaanreal <aryaantalukder@gmail.com>
2025-05-25 15:48:57 +0930

    Added movement logic

--- Player.cpp
+++ Player.cpp
@@ -3,9 +3,34 @@
 //constructor sets initial position, speed, and texture
 Player::Player(float startX, float startY, SDL_Texture* tex)
     : Entity(startX, startY, 5.0f), texture(tex) {}     //using base class constructor
-// handles players movement (to be done later)
-void Player::move() {
-    //add keyboard logic later
+
+
+//making player movement using wasd and the arrow keys
+
+void Player::handleInput(const Uint8* keystate) {
+    if(keystate[SDL_SCANCODE_LEFT]  || keystate[SDL_SCANCODE_A]) {
+
+        x -= speed;     //goes left
+    }
+    if(keystate[SDL_SCANCODE_RIGHT]  || keystate[SDL_SCANCODE_D]) {
+
+        x += speed;     //goes right
+    }
+    if(keystate[SDL_SCANCODE_UP]  || keystate[SDL_SCANCODE_W]) {
+
+        y -= speed;     //goes up
+    }
+    if(keystate[SDL_SCANCODE_DOWN]  || keystate[SDL_SCANCODE_S]) {
+
+        y += speed;     //goes down
+    }
+
+
+    //setting limiters so it doesn't go outside the given play area
+    if (x < 0) x = 0;
+    if (x > 800 - 64) x = 800-64;    //the player will be 64 bit in x
+    if (y < 0) y = 0;
+    if (y > 500 - 64) y = 500-64;     //the player will be 64 bit in y too
 }
 //draws the player sprite to the screen
 void Player::draw(SDL_Renderer*renderer){
--- Player.h
+++ Player.h
@@ -8,6 +8,7 @@ public:
  
      SDL_Texture* texture;   //texture used to render the player sprite
 
+     void handleInput(const Uint8* keystate);     //keyboard inputs are noww in
      Player(float startX, float startY, SDL_Texture* tex); //constructor to initialize position and texture
      void move() override;    //handles player movement (typically via keyboard input)
      void draw(SDL_Renderer* renderer) override;  //draws the player sprite to the screen using the renderer
