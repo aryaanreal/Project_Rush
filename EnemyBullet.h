512c9e276feaefeabc65ea1e5961922461762667
jithin <jithintomy2005@gmail.com>
2025-05-26 22:47:26 +0930

    Add: EnemyBullet class as subclass of Bullet for enemy projectiles

---
+++ EnemyBullet.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Bullet.h"
+
+//enemyBullet inherits from bullet and uses the same behavior
+class EnemyBullet : public Bullet {
+public:
+    //constructor calls base bullet constructor with speed and texture
+    EnemyBullet(float x, float y, float speed, SDL_Texture* tex)
+        : Bullet(x, y, speed, tex) {}
+};
