7c66747a7bc98ddefc3e552cb58d26576f94f069
aryaanreal <aryaantalukder@gmail.com>
2025-05-27 06:06:53 +0930

    Updated player so it supports the powerup system

--- Player.cpp
+++ Player.cpp
@@ -36,6 +36,30 @@ void Player::handleInput(const Uint8* keystate) {
 void Player::move() {
     //will add later
 }
+
+//player takes dmg
+void Player::takeDamage(int dmg) {
+    health -= dmg;
+    if (health < 0) health = 0;
+}
+
+void Player::applyUpgrade(PowerUpType type, BulletManager& BulletManager) {
+    switch (type)
+    {
+    case PowerUpType::SpeedBoost:
+        speed += 1.5f;
+        break;
+    case PowerUpType::MagBoost:
+        BulletManager.bulletsInMag += 10;
+        break;
+
+    case PowerUpType::BulletSpeed:
+        BulletManager.bulletSpeed += 2.0f;
+        if(BulletManager.bulletSpeed> 25.0f) BulletManager.bulletSpeed = 25.0f;
+    
+    break;
+    }
+}
 //draws the player sprite to the screen
 void Player::draw(SDL_Renderer*renderer){
     SDL_Rect dst = { (int)x, (int)y, 64,64}; //set position and size
--- Player.h
+++ Player.h
@@ -1,16 +1,23 @@
 #pragma once
 #include <SDL.h>
 #include "Entity.h"
+#include "PowerUp.h"
+#include "BulletManager.h"
+
 
 // defines the Player class, which handles position, movement, and rendering
 class Player : public Entity {
 public:
  
      SDL_Texture* texture;   //texture used to render the player sprite
+     int health = 100;        //player health
 
      void handleInput(const Uint8* keystate);     //keyboard inputs are noww in
      Player(float startX, float startY, SDL_Texture* tex); //constructor to initialize position and texture
      void move() override;    //handles player movement (typically via keyboard input)
      void draw(SDL_Renderer* renderer) override;  //draws the player sprite to the screen using the renderer
+
+     void takeDamage(int dmg); //player taking damage
+     void applyUpgrade(PowerUpType type, BulletManager& BulletManager);
     
 };
